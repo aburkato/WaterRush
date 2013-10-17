900485ecbecb8b15a87f8c8f1a10e01a86a002d3
AaronBuxbaum <ABux54@gmail.com>
2013-10-17 00:20:52 -0400

    Removed old bootstrap elements

--- js/index.js
+++
@@ -1,8 +0,0 @@
-    angular.module('PipesGame', ["ui.router"])
-    .config(function($stateProvider, $urlRouterProvider){
-      $stateProvider
-        .state('store', {
-            url: "/store",
-            templateUrl: "store.html"
-        })
-    });
