252da803f9b93597daee00a6b949a2fa62c413f8
tungpham31 <tungpham31@gmail.com>
2013-12-06 02:52:42 -0500

    Add a limit for the number of freeze power ups can be used.
    Signed-off-by: tungpham31 <tungpham31@gmail.com>

--- js/power_up.js
+++ js/power_up.js
@@ -1,17 +1,29 @@
 function FreezePowerUp(count) {
-	var count = count;
+	var num = count;
 	var freezing = false;
+
 	this.freeze = function() {
-		freezing = true;
-		setTimeout(unfreeze, 15 * 1000); //15 seconds
+		if (num > 0) {
+			num--;
+			this.display();
+			freezing = true;
+			setTimeout(unfreeze, 3 * 1000); //15 seconds
+		}	
 	};
+
 	unfreeze = function() {
 		$('#queue .pipe').each(function() {
 			$(this).css({"opacity": 1});
 		});
 		freezing = false;
 	};
+
 	this.isFreezing = function() {
 		return freezing;
 	};
+
+	this.display = function() {
+		$('#nFreeze').text(num);
+	};
+	this.display();
 }
